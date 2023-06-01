32bd9a2f5061fad414ae0a5b6ac428115af138e0
NKhateeja <117440639+NKhateeja@users.noreply.github.com>
2023-06-01 14:49:53 +0530

    Update TestScript1.java

--- src/test/java/Package/TestScript1.java
+++ src/test/java/Package/TestScript1.java
@@ -5,8 +5,6 @@ public class TestScript1 {
 	public static void main(String[] args) {
 		
 		//hello,welcome to project
-
-		//welcome to first pull
 		System.out.println("first change");
 		//thank you
 	}
